e2f2ba4f57b1a9a9082f248c493d5deb162d0feb
Jair Vasquez <jair1624@gmail.com>
2019-01-29 16:21:33 -0500

    Update sql
    Se añadió los insert como ejemplos

--- database/database.sql
+++ database/database.sql
@@ -10,4 +10,5 @@ CREATE TABLE game(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-DESCRIBE game;
+INSERT  INTO `game`(`id`,`title`,`description`,`image`,`created_at`) VALUES (2,'Clash of Clans','Game of SUPERCELL','https://pmcvariety.files.wordpress.com/2018/07/clash.png?w=1000&h=563&crop=1','2019-01-25 16:48:43');
+INSERT  INTO `game`(`id`,`title`,`description`,`image`,`created_at`) VALUES (4,'Clash Royale','Game of SUPERCELL','http://wrmx00.epimg.net/radio/imagenes/2017/10/10/tecnologia/1507658708_954109_1507659110_noticia_normal.jpg','2019-01-28 15:36:51');
